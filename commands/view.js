f0ba383913a765be44f2a907faed61a89c9f5a11
Unknown <anhim2@gmail.com>
2018-11-11 17:34:51 +0100

    adding view.js

---
+++ commands/view.js
@@ -0,0 +1,26 @@
+const { Command } = require('discord-akairo');
+
+class ViewCommand extends Command {
+	constructor() {
+		super('view', {
+			aliases: ['view', 'v'],
+		});
+	}
+
+	async exec(message) {
+		const gymList = await this.client.Gyms.findAll({ attributes: ['GymName', 'userIds'] });
+		const userGyms = [];
+		gymList.forEach(gym => {
+			console.log(gym);
+			if (gym.userIds.split(',').includes(message.author.id)) {
+				userGyms.push(gym.GymName);
+			}
+		});
+		return message.reply(`You currently have alerts for the following gyms:\n\`\`\`\n${userGyms.join('\n')}\`\`\``);
+
+		/* const tagString = tagList.map(t => t.name).join(', ') || 'No tags set.';
+		return message.channel.send(`List of tags: ${tagString}`); */
+	}
+}
+
+module.exports = ViewCommand;
