d041432730f1617c5e6a644dd9d725080057ac54
Unknown <anhim2@gmail.com>
2018-11-11 21:32:22 +0100

    tweaks to channelCreate for better handling

--- listeners/channelCreate.js
+++ listeners/channelCreate.js
@@ -23,9 +23,24 @@ class ChannelCreateListener extends Listener {
 				{ where : { GymName: channel_gym } },
 			);
 			if(affectedRows <= 0) console.log(`Error incrementing for gym ${channel_gym}`);
-			const users_arr = gym.userIds.split(',').map(id => `<@${id}>`);
 
-			this.client.channels.get('511235860625096726').send(`🔔🔔🔔\nBONG!\nA raid has just called for the gym \`${channel_gym}\`.\nConsider ye selves notified!\n🔔🔔🔔\n${users_arr.join(',')}\n\nIf you wish to no longer be notified for this gym, please type \`${config.prefix}remove ${channel_gym}\``, { split: true });
+			const send_chan = await this.client.Config.findOne({
+				where: { guildId: channel.guild.id },
+			});
+			// If there is nothing configured for this guild, do nothing
+			if(!send_chan) return console.log('No configs set, returning.');
+
+			setTimeout(async () => {
+				// This is run 20 seconds after channel create to give meowth time to post
+				const messages = await channel.messages.fetch();
+				const first = messages.last();
+
+				const author_id = first.mentions.users.first().id;
+
+				const users_arr = gym.userIds.split(',').filter(id => id != author_id).map(id => `<@${id}>`);
+
+				this.client.channels.get('511235860625096726').send(`🔔🔔🔔\nBONG!\nA raid has just called for the gym \`${channel_gym}\` in ${channel}.\nConsider ye selves notified!\n🔔🔔🔔\n${users_arr.join(',')}\n\nIf you wish to no longer be notified for this gym, please type \`${config.prefix}remove ${channel_gym}\``, { split: true });
+			}, 20000);
 		}
 		else {
 			console.log('moop');
