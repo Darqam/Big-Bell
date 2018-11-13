47f5d1de459bbe6683d89df9072a84e76c823717
Unknown <anhim2@gmail.com>
2018-11-13 21:18:46 +0100

    linting

--- commands/stats.js
+++ commands/stats.js
@@ -18,7 +18,6 @@ class StatsCommand extends Command {
 	async exec(message, args) {
 		if(!args.gym_name) return message.reply('No gyms found in query');
 		const gym_name = args.gym_name.split(',')[0].trim();
-		let output = '';
 
 		const gym = await this.client.Gyms.findOne({
 			where: {
@@ -26,15 +25,13 @@ class StatsCommand extends Command {
 			},
 		});
 		if(gym) {
-      const length = gym.userIds.split(',')[0] == '' ? 0 : gym.userIds.split(',').length;
-      const user = await this.client.users.fetch(gym.submittedById);
-      return message.channel.send(`Gym name: ${gym_name}\nAmount of people monitoring this gym: ${length}\nGym submitted by: ${user.tag}\nGym submission done on ${gym.submittedOn}\nGym list was pinged ${gym.timesPinged} times`);
+			const length = gym.userIds.split(',')[0] == '' ? 0 : gym.userIds.split(',').length;
+			const user = await this.client.users.fetch(gym.submittedById);
+			return message.channel.send(`Gym name: ${gym_name}\nAmount of people monitoring this gym: ${length}\nGym submitted by: ${user.tag}\nGym submission done on ${gym.submittedOn}\nGym list was pinged ${gym.timesPinged} times`);
 		}
-    else {
-      return message.reply(`Could not find a gym by the name of ${gym_name}.`)
-    }
-
-		return message.reply(output);
+		else {
+			return message.reply(`Could not find a gym by the name of ${gym_name}.`);
+		}
 	}
 }
 
