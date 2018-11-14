c930c3505ee150cd9287695dd35eaa3bfbd1bccf
Unknown <anhim2@gmail.com>
2018-11-14 12:21:47 +0100

    attempt at creating gyms+info from spreadsheet

--- commands/test.js
+++ commands/test.js
@@ -15,6 +15,18 @@ class StatsCommand extends Command {
 		const sheet_position = 6;
 		let sheet;
 
+		const gymList = await this.client.Gyms.findAll({ attributes: ['GymName', 'gymMap', 'gymDirections', 'exRaidNumber', 'exRaidEligibility'] });
+		if(!gymList) return message.channel.send('Could not query database. Aborting.');
+
+		const new_gyms = [];
+		const old_gyms = [];
+
+		const updated_maps = [];
+		const updated_directions = [];
+		const updated_ex_raid_number = [];
+		const updated_ex_raid_elig = [];
+
+
 		// Create a document object using the ID of the spreadsheet - obtained from its URL.
 		const doc = new GoogleSpreadsheet('1k6Gt4J323JnEB3oud_vK8gGyELoC0jha4etYXvFSbAk');
 
@@ -39,8 +51,102 @@ class StatsCommand extends Command {
 					offset: 1,
 					orderby: 'col2',
 				}, function(err, rows) {
+					if(!rows) return message.reply("Couldn't load any rows from that sheet.");
+					rows.forEach(async row => {
+						//console.log(row);
+						// row properties of interest
+						// gymname, exraideligibility, mapsurl, directionsurl, ofex
+						const local_gym = gymList.find(gym => gym.GymName == row.gymname);
+						if(local_gym) {
+							console.log(`Found local copy of gym for ${row.gymname}`);
+							if(local_gym.gymMap != row.mapsurl){
+
+							}
+							if(local_gym.gymDirections != row.directionsurl){
+
+							}
+							if(local_gym.exRaidNumber != row.ofex){
+
+							}
+							if(local_gym.exRaidEligibility != row.exraideligibility){
+
+							}
+						}
+						else {
+							new_gyms.push(row);
+							console.log(`Nope for ${row.gymname}`);
+						}
+					});
 					console.log(`Read ${rows.length} rows`);
-					console.log(rows.map(r => r.gymname));
+
+					if(new_gyms.length > 0){
+						// We have some new gyms that weren't in the database before
+						const msg = await message.channel.send(`I found ${new_gyms.length - 1} new gyms. Would you like to add the following gyms to the database?\n\`\`\`\n${new_gyms.join(', ')\n\`\`\``}`, { split: {maxLength: 1900, char: ',', prepend: '```\n', append: '\n```'} });
+						await msg.react('511174612323663874'); // check
+						await msg.react('511174899969032193'); // cross/X
+						valid_emojis = ['511174612323663874', '511174899969032193'];
+
+						const react_filter = (reaction, user) => {
+							return valid_emojis.includes(reaction.emoji.id) && !user.bot && user.id == message.author.id;
+						};
+						try {
+							const collected = await react_msg.awaitReactions(react_filter, { max: 1, time: 60000, errors: ['time'] });
+							const reaction = collected.first();
+
+							if(reaction.emoji.id == '511174612323663874') {
+								const success = [];
+								const error = [];
+								// If it's the check mark
+								for(let i = 0; i < new_gyms.length; i++) {
+
+									const date = new Date();
+									try {
+										const gym = await this.client.Gyms.create({
+											GymName: new_gyms[i].gymname.trim(),
+											userIds: '',
+											submittedById: message.author.id,
+											submittedOn: date.toString(),
+											timesPinged: 0,
+											gymMap: new_gyms[i].mapsurl,
+											gymDirections: new_gyms[i].directionsurl,
+											exRaidNumber: new_gyms[i].ofex,
+											exRaidEligibility: new_gyms[i].exraideligibility,
+										});
+										success.push(gym.GymName);
+									}
+									catch (e) {
+										if (e.name === 'SequelizeUniqueConstraintError') {
+											alreadyExists.push(gym_list[i]);
+										}
+										else {
+											console.log(e);
+											error.push(gym_list[i]);
+										}
+									}
+								}
+								// End of for loop over new gyms
+								let output = '';
+								if(success.length > 0) {
+									output += `Successfully created ${success.length} instances for: \n\`\`\`\n${success.join('\n')}\`\`\`\n`;
+								}
+
+								if(error.length > 0) {
+									output += `Could not create the gym instance for the following names: \n\`\`\`\n${error.join('\n')}\`\`\``;
+								}
+								return message.channel.send(output);
+							}
+							if(reaction.emoji.id == '511174899969032193') {
+								return message.channel.send('Got it, aborting.');
+							}
+
+						}
+						catch(e) {
+							console.log('Got no answer for gym precision, defaulting to highest match');
+							gym = results[0];
+							channel_gym = gym.GymName;
+						}
+					}
+					// End of "if there are new gyms"
 					step();
 				});
 			},
