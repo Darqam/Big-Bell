6366baa6c6ee1768237a12fc904926db7310644d
Unknown <anhim2@gmail.com>
2019-02-09 20:00:51 +0100

    add weekly view to stats + choice
    man is this shit ugly

--- commands/general/stats.js
+++ commands/general/stats.js
@@ -1,6 +1,7 @@
 const { Command } = require('discord-akairo');
 const { plotly_token, plotly_username } = require('../../config.json');
 const plotly = require('plotly')(plotly_username, plotly_token);
+const emojiCharacters = require('../../data/emojiCharacters.js');
 
 function compressArray(original) {
 	const compressed = [];
@@ -60,78 +61,99 @@ class StatsCommand extends Command {
 	}
 
 	async exec(message, { gym }) {
-		if(gym && gym.toLowerCase() == 'all') gym = null;
+		let option;
 
-		let allGyms;
-		if(!gym) {
-			allGyms = await message.client.Stats.findAll({
-				attributes: ['timestamp'],
-			});
-		}
-		else {
-			allGyms = await message.client.Stats.findAll({
-				attributes: ['timestamp'], where: {
-					gymName: gym.toLowerCase(),
-				},
-			});
-		}
-		if(gym && !allGyms) return message.channel.send('could not find a gym by that name');
+		const react_msg = await message.channel.send(`Please specify which type of stats you wish to see.\n${emojiCharacters[0]}: Historical\n${emojiCharacters[1]}: Weekly`);
+		const valid_emojis = [emojiCharacters[0], emojiCharacters[1]];
 
-		// Grab dates and format them for plotly to be happy
-		const gymTimes = allGyms.map(y => formatDate((new Date(parseInt(y.timestamp))).toUTCString()));
+		valid_emojis.forEach(async emj => {
+			await react_msg.react(emj);
+		});
 
-		// will format things of the style [{value: 'gymName', count: #}]
-		const compressed = compressArray(gymTimes);
-
-		const xAxis = compressed.map(time => time.value);
-		const yAxis = compressed.map(time => time.count);
-
-		// https://github.com/plotly/plotly-nodejs
-		const trace1 = {
-			x: xAxis,
-			y: yAxis,
-			mode: 'markers',
-			type: 'scatter',
+		const react_filter = (reaction, user) => {
+			return valid_emojis.includes(reaction.emoji.name) && user.id == message.author.id;
 		};
 
-		// Define the range to be one count above and below from max and min
-		const range = [Math.min(yAxis) - 1, Math.max(yAxis) + 1];
-		const layout = {
-			title: { text: gym ? gym : 'All gyms' },
-			xaxis: {
-				title: 'Raid callout dates',
-				autorange: true,
-				showline: true,
-			},
-			yaxis: {
-				title: '# of occurences',
-				range: range,
-				rangemode: 'tozero',
-				showline: true,
-			},
-			paper_bgcolor: 'rgba(230,230,230,0.9)',
-			plot_bgcolor: 'rgba(183,183,183,0.9)',
-		};
-
-		const figure = { 'data': [trace1], 'layout': layout };
-
-		const imgOpts = {
-			format: 'png',
-			width: 1000,
-			height: 500,
-		};
-
-		plotly.getImage(figure, imgOpts, async function(error, imageStream) {
-			if (error) {
-				message.channel.send('Something went horribly wrong with my plotting.');
-				return console.log (error);
+		const collector = react_msg.createReactionCollector(react_filter, { time: 20000 });
+		collector.on('collect', async (reaction) => {
+			for(let x = 0; x < valid_emojis.length; x++) {
+				if(valid_emojis[x] == reaction.emoji.name) option = x;
 			}
-			message.channel.send({
-				files: [{
-					attachment: imageStream,
-					name: '1.png',
-				}],
+
+			if(gym && gym.toLowerCase() == 'all') gym = null;
+
+			let allGyms;
+			if(!gym) {
+				allGyms = await message.client.Stats.findAll({
+					attributes: ['timestamp'],
+				});
+			}
+			else {
+				allGyms = await message.client.Stats.findAll({
+					attributes: ['timestamp'], where: {
+						gymName: gym.toLowerCase(),
+					},
+				});
+			}
+			if((gym && !allGyms) || allGyms.length == 0) return message.channel.send('Could not find a gym by that name');
+
+			message.channel.startTyping();
+			const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+			let gymTimes;
+			if(option == 0) gymTimes = allGyms.map(y => formatDate((new Date(parseInt(y.timestamp))).toUTCString()));
+			if(option == 1) gymTimes = allGyms.map(y => days[(new Date(parseInt(y.timestamp))).getDay()]);
+
+			const compressed = compressArray(gymTimes);
+
+			const xAxis = compressed.map(time => time.value);
+			const yAxis = compressed.map(time => time.count);
+
+			// https://github.com/plotly/plotly-nodejs
+			const trace1 = {
+				x: xAxis,
+				y: yAxis,
+				mode: 'markers',
+				type: 'scatter',
+			};
+
+			const range = [Math.min(yAxis) - 1, Math.max(yAxis) + 1];
+			const layout = {
+				title: { text: gym ? gym : 'All gyms' },
+				xaxis: {
+					title: 'Raid callout dates',
+					autorange: true,
+					showline: true,
+				},
+				yaxis: {
+					title: '# of occurences',
+					range: range,
+					rangemode: 'tozero',
+					showline: true,
+				},
+				paper_bgcolor: 'rgba(230,230,230,0.9)',
+				plot_bgcolor: 'rgba(183,183,183,0.9)',
+			};
+
+			const figure = { 'data': [trace1], 'layout': layout };
+
+			const imgOpts = {
+				format: 'png',
+				width: 1000,
+				height: 500,
+			};
+
+			plotly.getImage(figure, imgOpts, async function(error, imageStream) {
+				if (error) return console.log (error);
+
+				message.channel.send({
+					files: [{
+						attachment: imageStream,
+						name: '1.png',
+					}],
+				});
 			});
+			message.channel.stopTyping();
 		});
 	}
 }
