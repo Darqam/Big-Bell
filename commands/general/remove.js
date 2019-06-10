2fa90b39163945732380f61eda685225bbfa24b7
Unknown <anhim2@gmail.com>
2019-06-10 19:56:26 +0200

    Partial update of remove command

--- commands/general/remove.js
+++ commands/general/remove.js
@@ -6,12 +6,13 @@ class RemoveCommand extends Command {
 			aliases: ['remove', 'r', 'unwant'],
 			category: 'general',
 			description: {
-				content: 'Removes the author from the specified gym list.',
+				content: 'Removes the author from specified gym lists.',
 				usage: 'Gym Name 1, Gym Name 2, ...',
+				examples: ['Best Gym, Ok gym, ....', 'all'],
 			},
 			args: [
 				{
-					id: 'gym_list',
+					id: 'gymList',
 					match: 'content',
 					type: 'lowercase',
 				},
@@ -20,63 +21,89 @@ class RemoveCommand extends Command {
 	}
 
 	async exec(message, args) {
-		if(!args.gym_list) return message.reply('No gyms found in query');
+		if(!args.gymList) return message.reply('No gyms found in query');
 
-		let gym_list = args.gym_list.split(',');
-		gym_list = gym_list.map(x => x.trim());
+		let gymList = args.gymList.toLowerCase().split(',');
+		gymList = gymList.map(x => x.trim());
 
-		let user_gyms = [];
+		let userGyms = [];
 		let output = '';
 		const errors = [];
 		const success = [];
 		const noName = [];
 		let notPresent = [];
-		let all = false;
 
-		const all_gyms = await this.client.Gyms.findAll({ attributes: ['gymName', 'userIds'] });
-		all_gyms.forEach(gym => {
-			if (gym.userIds.split(',').includes(message.author.id)) {
-				user_gyms.push(gym);
+		const allGyms = await this.client.userGyms.findAll({ where: { userId: message.author.id } });
+
+		if(allGyms.length == 0) {
+			await message.react(message.client.myEmojiIds.failure);
+			return message.reply('I do not have you registered in any gyms');
+		}
+
+		if(gymList[0] === 'all') {
+			try {
+				await this.client.userGyms.Destroy({ where: { userId: message.author.id } });
 			}
-		});
-
-		if(gym_list[0] !== 'all') {
-			user_gyms = user_gyms.filter(aGym => gym_list.includes(aGym.gymName));
-			notPresent = gym_list.filter(choice => !user_gyms.map(x => x.gymName).includes(choice));
+			catch(e) {
+				console.log('Error in remove command', e);
+				return message.channel.send('There was a problem in removing your entries, please bring this to Daro\'s/Anhim\'s attention.');
+			}
 		}
 		else {
-			gym_list = user_gyms.map(x => x.gymName);
-			all = true;
-			if(user_gyms.length == 0) {
-				await message.react(message.client.myEmojiIds.failure);
-				return message.reply('I do not have you registered in any gyms');
+			// Go through the specified gym list and remove the associated ones.
+			for(let i = 0; i < allGyms.length; i++) {
+				try {
+					await this.client.userGyms.Destroy({
+						where: {
+							userId: message.author.id,
+							gymName: allGyms[i],
+						},
+					});
+					success.push(gymList[i]);
+				}
+				catch(e) {
+					console.log(`Error removing ${allGyms[i]} for ${message.author.tag}`, e);
+					errors.push(gymList[i]);
+				}
 			}
 		}
 
-		// At this point, user_gyms is either all gym objects the user is subscribed to (for the case of 'all')
+		// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		// noName.push(gymList[i]);
+		// This is still needed
+
+
+
+
+		if(gymList[0] !== 'all') {
+			userGyms = userGyms.filter(aGym => gymList.includes(aGym.gymName));
+			notPresent = gymList.filter(choice => !userGyms.map(x => x.gymName).includes(choice));
+		}
+
+		// At this point, userGyms is either all gym objects the user is subscribed to (for the case of 'all')
 		// or it is limited to the gyms who'se names were typed.
 
-		for(let i = 0; i < user_gyms.length; i++) {
-			if(user_gyms[i]) {
-				let user_list = user_gyms[i].userIds ? user_gyms[i].userIds.split(',') : [];
+		for(let i = 0; i < userGyms.length; i++) {
+			if(userGyms[i]) {
+				let user_list = userGyms[i].userIds ? userGyms[i].userIds.split(',') : [];
 				user_list = user_list.filter(e => e != message.author.id);
 
 				const affectedRows = await this.client.Gyms.update(
 					{ userIds: user_list.join(',') },
-					{ where : { gymName: gym_list[i] } },
+					{ where : { gymName: gymList[i] } },
 				);
 
 				if(affectedRows > 0) {
 					// If we managed to save the entry
-					success.push(gym_list[i]);
+					success.push(gymList[i]);
 				}
 				else {
 					// If there was a failure for some reason
-					errors.push(gym_list[i]);
+					errors.push(gymList[i]);
 				}
 			}
 			else {
-				noName.push(gym_list[i]);
+				noName.push(gymList[i]);
 			}
 		}
 		// End for loop
