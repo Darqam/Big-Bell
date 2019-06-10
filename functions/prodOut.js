67ba105a0a0a67a425752c1f5df66e25408740e3
Unknown <anhim2@gmail.com>
2019-06-10 13:00:24 +0200

    update prodOut to filter based on user selections

--- functions/prodOut.js
+++ functions/prodOut.js
@@ -1,3 +1,6 @@
+const raidMons = require('../data/raid_pokemons.json');
+const chanName = require('./isolateNames.js');
+
 module.exports = {
 	produceOut: async function(gym, channel, channel_gym, author_id) {
 		return new Promise(async (resolve) => {
@@ -80,7 +83,6 @@ module.exports = {
 			const nd = new Date(utc + (3600000 * timezone));
 			const curHour = nd.getHours();
 			const curMin = nd.getMinutes();
-			console.log(`${nd.getHours()}:${nd.getMinutes()}`);
 
 			let userArr = [];
 			userGyms.forEach(uGym => {
@@ -96,30 +98,55 @@ module.exports = {
 				if(start[0] > curHour || (start[0] == curHour && start[1] > curMin)) return;
 				// Oposite check for stop time
 				if(stop[0] < curHour || (stop[0] == curHour && stop[1] < curMin)) return;
+
+				// So from here on, we are in a valid time slot for relevant user
+
+				// Now, second filter, check if they have a limit of which raid levels
+				const raidLevels = uGym.raidLevels.split(',');
+
+				// channelInfo := [gym name, pokemon name ?, egg level ?]
+				const channelInfo = chanName.getChanGym(channel);
+				// If there are no extra info, toss it out.
+				if(!channelInfo[1] && !channelInfo[2]) return;
+
+				// If user raid level choices are not part of current raid level, abort
+				if(channelInfo[2] && !raidLevels.includes(channelInfo[2])) return;
+
+				// If there is no egg level but there is a pokemon name
+				if(!channelInfo[2] && channelInfo[1]) {
+					let valid = false;
+					const userMons = uGym.pokemons.split(',').map(p => p.toLowerCase());
+
+					// Loop over raidMons to see what level we have
+					for(const [key, value] of Object.entries(raidMons)) {
+						const lValue = value.map(v => v.toLowerCase());
+
+						// If the raid level matches the user's list, and the pokemon is found inside it
+						if(raidLevels.includes(key) && lValue.includes(channelInfo[1].toLowerCase())) {
+							// If the user has no pokemon list, or if the list includes this pokemon
+							if(userMons.length == 0 || userMons.includes(channelInfo[1].toLowerCase())) {
+								valid = true;
+								break;
+							}
+						}
+					}
+					// If we finished the loop and found nothing
+					if(!valid) return;
+				}
+				// All checks have passed, add user to mention list.
 				userArr.push(uGym.userId);
 			});
-			// !!!!!!!!!!!!!!!!!!!!!!!!!!!
-			// !!!!!!!!!!!!!!!!!!!!!!!!!!!
-			// !!!!!!!!!!!!!!!!!!!!!!!!!!!
-			// !!!!!!!!!!!!!!!!!!!!!!!!!!!
-			// !!!!!!!!!!!!!!!!!!!!!!!!!!!
-			// !!!!!!!!!!!!!!!!!!!!!!!!!!!
-			// !!!!!!!!!!!!!!!!!!!!!!!!!!!
-			// !!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-			if(author_id) {
-				userArr = userArr.filter(id => id != author_id).map(id => `<@${id}>`);
-			}
-			else {
-				userArr = userArr.map(id => `<@${id}>`);
-			}
 			// If there are no users for this gym, stop
 			if(userArr.length < 1) return console.log(`No users to ping for ${channel_gym}.`);
+
+			if(author_id) userArr = userArr.filter(id => id != author_id).map(id => `<@${id}>`);
+			else userArr = userArr.map(id => `<@${id}>`);
 
 			// Since this has the potential to be a massive message, tell
 			// djs to split the message at ~1900 characters and split by the
 			// comma character which will be in between each mention.
-			const final_return = `🔔🔔🔔 ${userArr.join(',')} 🔔🔔🔔\nIf you wish to no longer be notified for this gym, please type \`${config.prefixes.split(',')[0]}remove ${channel_gym}\``;
+			const final_return = `🔔🔔🔔 ${userArr.join(',')} 🔔🔔🔔\nIf you wish to no longer be notified for this gym, please use the \`disable\` or \`remove\` command.`;
 
 			const return_array = [final_return, channel_gym, disabled];
 			resolve(return_array);
