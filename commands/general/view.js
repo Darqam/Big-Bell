3fdb1d1fef36a54337e498c0c6d16c893237a711
Unknown <anhim2@gmail.com>
2019-06-11 13:54:14 +0200

    alter view

--- commands/general/view.js
+++ commands/general/view.js
@@ -13,13 +13,9 @@ class ViewCommand extends Command {
 	}
 
 	async exec(message) {
-		const gymList = await this.client.Gyms.findAll({ attributes: ['gymName', 'userIds'] });
-		const userGyms = [];
-		gymList.forEach(gym => {
-			if (gym.userIds.split(',').includes(message.author.id)) {
-				userGyms.push(gym.gymName);
-			}
-		});
+		const gymList = await this.client.userGyms.findAll({ where: { userId: message.author.id }	});
+		const userGyms = gymList.map(gym => gym.gymName);
+
 		if(userGyms.length > 0) return message.reply(`You currently have alerts for the following gyms:\n\`\`\`\n${userGyms.join('\n')}\`\`\``);
 		return message.reply('You currently are registered to recieve no alerts.');
 	}
