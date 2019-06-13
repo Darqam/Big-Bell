b4d1ba921b817065f9cb08867b088f7bd3ece3b4
Unknown <anhim2@gmail.com>
2019-06-14 00:31:32 +0200

    base file for correct
    totally not copy pasted from ping

---
+++ commands/general/correct.js
@@ -0,0 +1,24 @@
+const { Command } = require('discord-akairo');
+
+class ChangeCommand extends Command {
+	constructor() {
+		super('change', {
+			aliases: ['correct', 'fix'],
+			category: 'general',
+			description: {
+				content: 'Allows to edit which gym is recognized by Victreebel.',
+				usage: '',
+			},
+		});
+	}
+
+	exec(message) {
+		return message.channel.send('Pong!').then(sent => {
+			const timeDiff = (sent.editedAt || sent.createdAt) - (message.editedAt || message.createdAt);
+			const text = `🔂\u2000**RTT**: ${timeDiff} ms\n💟\u2000**Heartbeat**: ${Math.round(sent.client.ws.ping)} ms`;
+			return sent.edit(`Pong!\n${text}`);
+		});
+	}
+}
+
+module.exports = ChangeCommand;
